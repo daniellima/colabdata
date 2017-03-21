4b36619a74ce04dc7e27eb3b836d5f91c1b3c13b
tnik <daniellima.pessoal@gmail.com>
2017-03-21 12:05:23 +0000

    refactor para deixar o urls.py mais facil de relacionar com a aplicação como está agora

--- image_tagger/urls.py
+++ image_tagger/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^datasets$', views.datasets, name='datasets'),
+    url(r'^private_datasets$', views.private_datasets, name='private_datasets'),
+    url(r'^dataset/([0-9]+)/publications/$', views.dataset_publications, name="dataset_publications"),
+    url(r'^dataset/([0-9]+)/image_tagger$', views.dataset_image_tagger, name='dataset_image_tagger'),
     url(r'^([0-9]+)/([0-9]+)$', views.image, name='image'),
     url(r'^save/tag$', views.save_tag, name='save_tag'),
-    url(r'^save/relation$', views.save_relation, name='save_relation'),
     url(r'^delete/tag$', views.delete_tag, name='delete_tag'),
+    url(r'^save/relation$', views.save_relation, name='save_relation'),
     url(r'^delete/relation$', views.delete_relation, name='delete_relation'),
-    url(r'^logout$', views.logout, name='logout'),
-    url(r'^dataset/([0-9]+)/publications/$', views.dataset_publications, name="dataset_publications"),
-    url(r'^datasets$', views.datasets, name='datasets'),
-    url(r'^$', views.index, name='index'),
-    url(r'^private_datasets$', views.private_datasets, name='private_datasets'),
-    url(r'^dataset/([0-9]+)/image_tagger$', views.dataset_image_tagger, name='dataset_image_tagger'),
 ]
