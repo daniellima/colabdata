d94e66be3c291d648d4313718873e5fda1e0c6c1
tnik <daniellima.pessoal@gmail.com>
2017-03-21 17:58:01 +0000

    Corrigindo bug em que o decorator não passava corretamente os parametros

--- image_tagger/decorators.py
+++ image_tagger/decorators.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 
 def ajax_aware_login_required(f):
     
-    def decoration(request):
+    def decoration(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return f(request)
+            return f(request, *args, **kwargs)
         else:
             return HttpResponse(status=401)
             
