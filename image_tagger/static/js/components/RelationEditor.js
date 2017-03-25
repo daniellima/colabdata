5f8f3bd3ab4923932ca0cc0955ac04a469fee827
tnik <daniellima.pessoal@gmail.com>
2017-03-25 22:22:40 +0000

    Melhorando o feedback de erro no editor de relação e adicionando validação para relação repetida

--- image_tagger/static/js/components/RelationEditor.js
+++ image_tagger/static/js/components/RelationEditor.js
@@ -4,6 +4,8 @@ var component = RelationEditorComponent = function($http, $rootScope){
 
     this.open = false;
     
+    this.errors = [];
+    
     this.relationBeingEdited = null;
     this.originTag = null;
     this.targetTag = null;
@@ -50,6 +52,35 @@ component.prototype = {
 
     $onChanges: function(changes){},
     
+    checkValid: function() {
+        this.errors = [];
+        if(this.hasEmptyFields()) {
+            this.errors.push("Some fields are empty. Choose two tags and one name for the relation.");
+        }
+        if(this.hasRelationThatAlreadyExists()) {
+            this.errors.push("This relation already exists.");
+        }
+        
+        return this.errors.length == 0;
+    },
+    
+    hasEmptyFields: function() {
+        return !(this.originTag && this.targetTag && this.name !== null && this.name !== "");
+    },
+    
+    hasRelationThatAlreadyExists: function() {
+        var relations = store.getAllRelations();
+        for(var i = 0; i < relations.length; i++) {
+            var relation = relations[i];
+            if(this.relationBeingEdited !== null && this.relationBeingEdited.id == relation.id) continue;
+            if(relation.originTag == this.originTag && relation.targetTag == this.targetTag && this.name == relation.name) {
+                return true;
+            }
+        }
+        
+        return false;
+    },
+    
     setOpen: function(value) {
         if(this.open == true && value == false){
             this.$rootScope.$emit('modal-closed');
@@ -59,14 +90,6 @@ component.prototype = {
         }
         
         this.open = value;
-    },
-    
-    isBlocksChoosen: function(){
-        return this.originTag && this.targetTag;
-    },
-    
-    isRelationComplete: function(){
-        return this.originTag && this.targetTag && this.name !== null && this.name !== "";
     },
     
     tagThumbnailButtonClickHandler: function(whichTag) {
@@ -95,18 +118,22 @@ component.prototype = {
     },
     
     saveButtonClickHandler: function() {
-        showLoadingOverlay(true, "Salvando...");
-        store.saveRelation(this.$http, this.relationBeingEdited, this.name, this.originTag, this.targetTag)
-        .then(function(){
-            
-            this.modalCallback();
-            
-            this.setOpen(false);
-        }.bind(this), function(response) {
-            showAndLogErrorThatOcurredDuringAction("salvar a relação", response, this.$rootScope);
-        }.bind(this))
-        .finally(function(){
-            showLoadingOverlay(false);
-        });
+        var isValid = this.checkValid();
+        
+        if(isValid) {
+            showLoadingOverlay(true, "Salvando...");
+            store.saveRelation(this.$http, this.relationBeingEdited, this.name, this.originTag, this.targetTag)
+            .then(function(){
+                
+                this.modalCallback();
+                
+                this.setOpen(false);
+            }.bind(this), function(response) {
+                showAndLogErrorThatOcurredDuringAction("salvar a relação", response, this.$rootScope);
+            }.bind(this))
+            .finally(function(){
+                showLoadingOverlay(false);
+            });
+        }
     }
 };
